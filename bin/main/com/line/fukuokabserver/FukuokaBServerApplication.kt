6a62d971a5186059ed445e282d01450e5c373053
Po <colagarychen@gmail.com>
2018-08-13 16:52:06 +0900

    Project initialization

---
+++ bin/main/com/line/fukuokabserver/FukuokaBServerApplication.kt
@@ -0,0 +1,11 @@
+package com.line.fukuokabserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FukuokaBServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<FukuokaBServerApplication>(*args)
+}
